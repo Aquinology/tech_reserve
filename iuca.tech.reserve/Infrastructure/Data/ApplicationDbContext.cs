d68dcaa21da709306a284272079e5c04f2c63025
Aquinology <riabova_e@iuca.kg>
2025-01-20 14:38:24 +0600

    Add db connection and identity

---
+++ iuca.tech.reserve/Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Application.Interfaces.Common;
+
+namespace Infrastructure.Data;
+
+public class ApplicationDbContext : IdentityDbContext<IdentityUser>, IApplicationDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+}
